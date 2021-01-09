167e33fbc0a2a0a95ec9d1a74716c81c90652415
Jorge Tesch <jorge.tesch@users.noreply.github.com>
2021-01-09 19:13:39 +0100

    .

--- ProyectoFinal/src/main.java
+++ ProyectoFinal/src/main.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 public class main {
 	public static void main(String argv[]) throws FileNotFoundException, IOException{
 		LecturaFicheros.AllLecture("C:\\Users\\jorge\\pruebaPOO.txt");
-		GestorPartida.instanciarCreencias();
-		//GestorPartida.getJugadores()[0].setSala("Jardin");
-		//GestorPartida.getJugadores()[1].setSala("Jardin");
-		GestorPartida.actualizarCreencias(0);
-		GUI interfaz = new GUI(0);
 		
-		System.out.println(GestorPartida.getSalas()[1].getSalasVecinas().length);
+		GestorPartida.instanciarCreencias();
+		GestorPartida.actualizarCreencias(0);
+		
+		GUI interfaz = new GUI(0);
 	
 	}
 }
